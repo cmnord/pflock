dd8a9a2c8cee133ee8e0b2d0e7e2424a5ed6b98a
Claire Nord <cnord@mit.edu>
2020-07-23 02:26:13 -0400

    lib: simplify logic and allocations

--- src/lib.rs
+++ src/lib.rs
@@ -25,14 +25,12 @@ impl PFLock {
     }
 
     pub fn read_lock(&self) {
-        let w: usize;
-
         // Increment the rin count and read the writer bits
-        w = self.rin.fetch_add(RINC, Ordering::SeqCst) & WBITS;
+        let w = self.rin.fetch_add(RINC, Ordering::SeqCst) & WBITS;
 
         // Spin (wait) if there is a writer present (w != 0), until either PRES
         // and/or PHID flips
-        while !(w == 0 || w != (self.rin.load(Ordering::SeqCst) & WBITS)) {
+        while (w != 0) && (w == (self.rin.load(Ordering::SeqCst) & WBITS)) {
             spin_loop_hint();
         }
     }
@@ -43,21 +41,18 @@ impl PFLock {
     }
 
     pub fn write_lock(&self) {
-        let mut ticket: usize;
-        let w: usize;
-
         // Wait until it is my turn to write-lock the resource
-        ticket = self.win.fetch_add(1, Ordering::SeqCst);
-        while !(ticket == self.wout.load(Ordering::SeqCst)) {
+        let wticket = self.win.fetch_add(1, Ordering::SeqCst);
+        while wticket != self.wout.load(Ordering::SeqCst) {
             spin_loop_hint();
         }
 
         // Set the write-bits of rin to indicate this writer is here
-        w = PRES | (ticket & PHID);
-        ticket = self.rin.fetch_add(w, Ordering::SeqCst);
+        let w = PRES | (wticket & PHID);
+        let rticket = self.rin.fetch_add(w, Ordering::SeqCst);
 
         // Wait until all current readers have finished (i.e. rout catches up)
-        while !(ticket == self.rout.load(Ordering::SeqCst)) {
+        while rticket != self.rout.load(Ordering::SeqCst) {
             spin_loop_hint();
         }
     }
