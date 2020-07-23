d9256e7f3d7fd50a9b753948ffa01c583770dab1
Claire Nord <cnord@mit.edu>
2020-07-23 01:01:38 -0400

    lib: add comments from Optimizing the Common Case
    Comments from the C implementation in the code supplement for the paper
    "Real-time multiprocessor locks with nesting: Optimizing the common case".
    
    @article{nemitz2019real,
      title={Real-time multiprocessor locks with nesting: Optimizing the common case},
      author={Nemitz, Catherine E and Amert, Tanya and Anderson, James H},
      journal={Real-Time Systems},
      volume={55},
      number={2},
      pages={296--348},
      year={2019},
      publisher={Springer}
    }

--- src/lib.rs
+++ src/lib.rs
@@ -26,33 +26,48 @@ impl PFLock {
 
     pub fn read_lock(&self) {
         let w: usize;
+
+        // Increment the rin count and read the writer bits
         w = self.rin.fetch_add(RINC, Ordering::SeqCst) & WBITS;
+
+        // Spin (wait) if there is a writer present (w != 0), until either PRES
+        // and/or PHID flips
         while !(w == 0 || w != (self.rin.load(Ordering::SeqCst) & WBITS)) {
             spin_loop_hint();
         }
     }
 
     pub fn read_unlock(&self) {
+        // Increment rout to mark the read-lock returned
         self.rout.fetch_add(RINC, Ordering::SeqCst);
     }
 
     pub fn write_lock(&self) {
         let mut ticket: usize;
         let w: usize;
+
+        // Wait until it is my turn to write-lock the resource
         ticket = self.win.fetch_add(1, Ordering::SeqCst);
         while !(ticket == self.wout.load(Ordering::SeqCst)) {
             spin_loop_hint();
         }
+
+        // Set the write-bits of rin to indicate this writer is here
         w = PRES | (ticket & PHID);
         ticket = self.rin.fetch_add(w, Ordering::SeqCst);
+
+        // Wait until all current readers have finished (i.e. rout catches up)
         while !(ticket == self.rout.load(Ordering::SeqCst)) {
             spin_loop_hint();
         }
     }
 
     pub fn write_unlock(&self) {
-        // zero the least-significant byte of rin
+        // Clear the least-significant byte of rin
         self.rin.fetch_and(ZERO_MASK, Ordering::SeqCst);
+
+        // Increment wout to indicate this write has released the lock
+        // Only one writer should ever be here
         self.wout.fetch_add(1, Ordering::SeqCst);
     }
 }
