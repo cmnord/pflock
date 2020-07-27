b45503a03b019753feaa123e5d66d4c582ccfc57
Claire Nord <cnord@mit.edu>
2020-07-27 19:21:02 -0400

    tests read_write: print more timing info

--- tests/read_write.rs
+++ tests/read_write.rs
@@ -1,42 +1,41 @@
+#![feature(div_duration)]
+
 use pflock::PFLock;
 use std::sync::Arc;
 use std::thread;
 use std::time::{Duration, Instant};
 
-const NUM_ITERATIONS: usize = 100;
+const NUM_ITERATIONS: usize = 40;
 
-fn read_serial(sleep_duration: Duration) {
+fn read_serial(sleep_duration: Duration) -> Duration {
     let lock = Arc::new(PFLock::new());
 
     let now = Instant::now();
-    for i in 0..NUM_ITERATIONS {
+    for _ in 0..NUM_ITERATIONS {
         let arc_clone = Arc::clone(&lock);
         thread::spawn(move || {
             arc_clone.read_lock();
             thread::sleep(sleep_duration);
-            println!("rs{}", i);
             arc_clone.read_unlock();
         })
         .join()
         .unwrap();
     }
 
-    let elapsed = now.elapsed();
-    println!("read serial    = {:?}", elapsed);
+    now.elapsed()
 }
 
-fn read_parallel(sleep_duration: Duration) {
+fn read_parallel(sleep_duration: Duration) -> Duration {
     let lock = Arc::new(PFLock::new());
 
     let mut threads = vec![];
 
     let now = Instant::now();
-    for i in 0..NUM_ITERATIONS {
+    for _ in 0..NUM_ITERATIONS {
         let arc_clone = Arc::clone(&lock);
         threads.push(thread::spawn(move || {
             arc_clone.read_lock();
             thread::sleep(sleep_duration);
-            println!("rp{}", i);
             arc_clone.read_unlock();
         }));
     }
@@ -45,42 +44,39 @@ fn read_parallel(sleep_duration: Duration) {
     for t in threads {
         let _ = t.join();
     }
-    let elapsed = now.elapsed();
-    println!("read parallel  = {:?}", elapsed);
+
+    now.elapsed()
 }
 
-fn write_serial(sleep_duration: Duration) {
+fn write_serial(sleep_duration: Duration) -> Duration {
     let lock = Arc::new(PFLock::new());
 
     let now = Instant::now();
-    for i in 0..NUM_ITERATIONS {
+    for _ in 0..NUM_ITERATIONS {
         let arc_clone = Arc::clone(&lock);
         thread::spawn(move || {
             arc_clone.write_lock();
             thread::sleep(sleep_duration);
-            println!("ws{}", i);
             arc_clone.write_unlock();
         })
         .join()
         .unwrap();
     }
 
-    let elapsed = now.elapsed();
-    println!("write serial   = {:?}", elapsed);
+    now.elapsed()
 }
 
-fn write_parallel(sleep_duration: Duration) {
+fn write_parallel(sleep_duration: Duration) -> Duration {
     let lock = Arc::new(PFLock::new());
 
     let mut threads = vec![];
 
     let now = Instant::now();
-    for i in 0..NUM_ITERATIONS {
+    for _ in 0..NUM_ITERATIONS {
         let arc_clone = Arc::clone(&lock);
         threads.push(thread::spawn(move || {
             arc_clone.write_lock();
             thread::sleep(sleep_duration);
-            println!("wp{}", i);
             arc_clone.write_unlock();
         }));
     }
@@ -89,19 +85,39 @@ fn write_parallel(sleep_duration: Duration) {
     for t in threads {
         let _ = t.join();
     }
-    let elapsed = now.elapsed();
-    println!("write parallel = {:?}", elapsed);
+
+    now.elapsed()
 }
 
 #[test]
 fn read_write() {
-    let sleep_duration = Duration::from_millis(100);
+    let sleep_duration = Duration::from_millis(50);
     println!(
         "sleep {:?} in each thread, {} iterations",
         sleep_duration, NUM_ITERATIONS
     );
-    read_serial(sleep_duration);
-    read_parallel(sleep_duration);
-    write_serial(sleep_duration);
-    write_parallel(sleep_duration);
+
+    let rs = read_serial(sleep_duration);
+    println!("read serial    = {:?}", rs);
+
+    let rp = read_parallel(sleep_duration);
+    println!("read parallel  = {:?}", rp);
+
+    println!(
+        "read parallelism: -{:?} change, {:.1}x faster",
+        rs - rp,
+        rs.div_duration_f64(rp)
+    );
+
+    let ws = write_serial(sleep_duration);
+    println!("write serial   = {:?}", ws);
+
+    let wp = write_parallel(sleep_duration);
+    println!("write parallel = {:?}", wp);
+
+    println!(
+        "write parallelism: +{:?} change, {:.3}% as fast",
+        wp - ws,
+        ws.div_duration_f64(wp)
+    );
 }
