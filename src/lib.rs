966072d5d79548ee6eb6aa354c6c0883b885cd6e
Claire Nord <cnord@mit.edu>
2020-07-31 14:36:49 -0400

    lib: relax all ordering to Relaxed

--- src/lib.rs
+++ src/lib.rs
@@ -32,44 +32,44 @@ impl PFLock {
 
     pub fn read_lock(&self) {
         // Increment the rin count and read the writer bits
-        let w = self.rin.fetch_add(RINC, Ordering::SeqCst) & WBITS;
+        let w = self.rin.fetch_add(RINC, Ordering::Relaxed) & WBITS;
 
         // Spin (wait) if there is a writer present (w != 0), until either PRES
         // and/or PHID flips
-        while (w != 0) && (w == (self.rin.load(Ordering::SeqCst) & WBITS)) {
+        while (w != 0) && (w == (self.rin.load(Ordering::Relaxed) & WBITS)) {
             spin_loop_hint();
         }
     }
 
     pub fn read_unlock(&self) {
         // Increment rout to mark the read-lock returned
-        self.rout.fetch_add(RINC, Ordering::SeqCst);
+        self.rout.fetch_add(RINC, Ordering::Relaxed);
     }
 
     pub fn write_lock(&self) {
         // Wait until it is my turn to write-lock the resource
-        let wticket = self.win.fetch_add(1, Ordering::SeqCst);
-        while wticket != self.wout.load(Ordering::SeqCst) {
+        let wticket = self.win.fetch_add(1, Ordering::Relaxed);
+        while wticket != self.wout.load(Ordering::Relaxed) {
             spin_loop_hint();
         }
 
         // Set the write-bits of rin to indicate this writer is here
         let w = PRES | (wticket & PHID);
-        let rticket = self.rin.fetch_add(w, Ordering::SeqCst);
+        let rticket = self.rin.fetch_add(w, Ordering::Relaxed);
 
         // Wait until all current readers have finished (i.e. rout catches up)
-        while rticket != self.rout.load(Ordering::SeqCst) {
+        while rticket != self.rout.load(Ordering::Relaxed) {
             spin_loop_hint();
         }
     }
 
     pub fn write_unlock(&self) {
         // Clear the least-significant byte of rin
-        self.rin.fetch_and(ZERO_MASK, Ordering::SeqCst);
+        self.rin.fetch_and(ZERO_MASK, Ordering::Relaxed);
 
         // Increment wout to indicate this write has released the lock
         // Only one writer should ever be here
-        self.wout.fetch_add(1, Ordering::SeqCst);
+        self.wout.fetch_add(1, Ordering::Relaxed);
     }
 }
 
